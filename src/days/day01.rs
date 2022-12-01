5d20210cd40be22182e762e1f1744bf89e4c40d2
beeb <vbersier@gmail.com>
2022-12-01 07:18:15 +0100

    feat: day 1 part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,25 +1,33 @@
-use nom::IResult;
+use nom::{
+    character::complete::{line_ending, u16},
+    combinator::map,
+    multi::{count, separated_list0},
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day01;
 
 impl Day for Day01 {
-    type Input = String;
+    type Input = Vec<Vec<usize>>;
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(
+            count(line_ending, 2),
+            separated_list0(line_ending, map(u16, |c| c as usize)),
+        )(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input.iter().map(|e| e.iter().sum()).max().unwrap_or(0)
     }
 
     type Output2 = usize;
 
     fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+        0
     }
 }
