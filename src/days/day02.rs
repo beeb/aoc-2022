0b44ba07da8f1e07b696acf19dcefd1e8e5e1089
beeb <vbersier@gmail.com>
2022-12-02 07:02:35 +0100

    feat: day 1 part 1

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,20 +1,60 @@
-use nom::IResult;
+use nom::{
+    branch::alt,
+    character::complete::{char, line_ending, space1},
+    combinator::map,
+    multi::separated_list0,
+    sequence::tuple,
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day02;
 
-impl Day for Day02 {
-    type Input = String;
+fn parse_char(input: char) -> isize {
+    match input {
+        'A' | 'X' => 1,
+        'B' | 'Y' => 2,
+        'C' | 'Z' => 3,
+        _ => unreachable!(),
+    }
+}
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+fn parse_chars(input: &[(char, char)]) -> impl Iterator<Item = (isize, isize)> + '_ {
+    input.iter().map(|(a, x)| (parse_char(*a), parse_char(*x)))
+}
+
+impl Day for Day02 {
+    type Input = Vec<(char, char)>;
+
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(
+            line_ending,
+            map(
+                tuple((
+                    alt((char('A'), char('B'), char('C'))),
+                    space1,
+                    alt((char('X'), char('Y'), char('Z'))),
+                )),
+                |(a, _, x)| (a, x),
+            ),
+        )(input)
     }
 
-    type Output1 = usize;
+    type Output1 = isize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        let mut score = 0;
+        for (elf, me) in parse_chars(input) {
+            score += match me - elf {
+                2 | -1 => 0,
+                -2 | 1 => 6,
+                0 => 3,
+                _ => unreachable!(),
+            };
+            score += me;
+        }
+        score
     }
 
     type Output2 = usize;
