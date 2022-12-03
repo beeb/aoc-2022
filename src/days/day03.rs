2ef7a60688ff37ad4c86e0bc9e7f35c91cca84e5
beeb <vbersier@gmail.com>
2022-12-03 09:28:29 +0100

    feat(day3): part 1

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,20 +1,44 @@
-use nom::IResult;
+use itertools::Itertools;
+use nom::{
+    character::complete::{alpha1, line_ending},
+    multi::separated_list0,
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day03;
 
-impl Day for Day03 {
-    type Input = String;
+fn parse_line(input: &str) -> IResult<&str, (Vec<u8>, Vec<u8>)> {
+    let (rest, s) = alpha1(input)?;
+    let chars: Vec<u8> = s
+        .chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                return (c as u8) - 38;
+            }
+            (c as u8) - 96
+        })
+        .collect();
+    let mid = chars.len() / 2;
+    Ok((rest, (chars[..mid].to_vec(), chars[mid..].to_vec())))
+}
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+impl Day for Day03 {
+    type Input = Vec<(Vec<u8>, Vec<u8>)>;
+
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(line_ending, parse_line)(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input
+            .iter()
+            .flat_map(|rs| rs.0.iter().filter(|e| rs.1.contains(e)).dedup())
+            .map(|e| *e as usize)
+            .sum::<usize>()
     }
 
     type Output2 = usize;
