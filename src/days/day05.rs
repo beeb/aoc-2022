6fcff8ade57c08643cdfacd741e683f0115c0a69
Valentin Bersier <vbersier@gmail.com>
2022-12-05 10:00:00 +0100

    perf(day5): avoid cloning list with interior mutability

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{cell::RefCell, collections::VecDeque};
 
 use nom::{
     branch::alt,
@@ -26,7 +26,7 @@ pub struct Move {
 #[derive(Debug, Clone, Default)]
 pub struct State {
     /// Each stack is represented as a double-ended queue, where the front is the bottom and the back is the top
-    pub stacks: [VecDeque<char>; NUM_STACKS],
+    pub stacks: RefCell<[VecDeque<char>; NUM_STACKS]>,
     pub moves: Vec<Move>,
 }
 
@@ -43,16 +43,18 @@ impl State {
     }
 
     /// Fill the stacks with the input data, from the front so the order is preserved
-    pub fn push_crate_front(&mut self, stack: usize, id: char) -> &mut Self {
-        self.stacks.get_mut(stack).unwrap().push_front(id);
+    pub fn push_crate_front(&self, stack: usize, id: char) -> &Self {
+        let mut stacks = self.stacks.borrow_mut();
+        stacks.get_mut(stack).unwrap().push_front(id);
         self
     }
 
     /// In the first part, we move the crates one-by-one, so pop from the back (top) and push to the back (top)
-    pub fn move_crates_9000(&mut self, amount: usize, from: usize, to: usize) -> &mut Self {
+    pub fn move_crates_9000(&self, amount: usize, from: usize, to: usize) -> &Self {
+        let mut stacks = self.stacks.borrow_mut();
         for _ in 0..amount {
-            let c = self.stacks[from - 1].pop_back().unwrap();
-            self.stacks[to - 1].push_back(c);
+            let c = stacks[from - 1].pop_back().unwrap();
+            stacks[to - 1].push_back(c);
         }
         self
     }
@@ -60,10 +62,11 @@ impl State {
     /// In the second part, we move the crates in batches of `amount`.
     ///
     /// Cleaner-looking version where an intermediary VecDeque is created to hold the moved crates.
-    pub fn move_crates_9001b(&mut self, amount: usize, from: usize, to: usize) -> &mut Self {
-        let from_len = self.stacks[from - 1].len();
-        let mut mov = self.stacks[from - 1].split_off(from_len - amount);
-        self.stacks[to - 1].append(&mut mov);
+    pub fn move_crates_9001b(&self, amount: usize, from: usize, to: usize) -> &Self {
+        let mut stacks = self.stacks.borrow_mut();
+        let from_len = stacks[from - 1].len();
+        let mut mov = stacks[from - 1].split_off(from_len - amount);
+        stacks[to - 1].append(&mut mov);
         self
     }
 
@@ -74,14 +77,15 @@ impl State {
     /// we use `split_at_mut`, which gives two mutable references to different parts of the slice.
     /// We choose to split at the highest index between `from` and `to`, so that the first element in the
     /// second slice is the stack referring to max(from, to) and both stacks of interest are in different slices.
-    pub fn move_crates_9001(&mut self, amount: usize, from: usize, to: usize) -> &mut Self {
+    pub fn move_crates_9001(&self, amount: usize, from: usize, to: usize) -> &Self {
+        let mut stacks = self.stacks.borrow_mut();
         if from > to {
-            let (half_to, half_from) = self.stacks.split_at_mut(from - 1);
+            let (half_to, half_from) = stacks.split_at_mut(from - 1);
             let from_stack = half_from.first_mut().unwrap();
             let to_stack = half_to.get_mut(to - 1).unwrap();
             move_crates_with_stacks(from_stack, to_stack, amount);
         } else {
-            let (half_from, half_to) = self.stacks.split_at_mut(to - 1);
+            let (half_from, half_to) = stacks.split_at_mut(to - 1);
             let to_stack = half_to.first_mut().unwrap();
             let from_stack = half_from.get_mut(from - 1).unwrap();
             move_crates_with_stacks(from_stack, to_stack, amount);
@@ -146,21 +150,23 @@ impl Day for Day05 {
 
     /// Part 1 took 0.020906ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        let mut state = input.clone(); // get a copy of the state
-        for m in state.moves.clone() {
+        let state = input.clone(); // get a copy of the state
+        for m in &state.moves {
             state.move_crates_9000(m.amount, m.from, m.to);
         }
-        state.stacks.iter().map(|s| s.back().unwrap()).collect()
+        let stacks = state.stacks.borrow();
+        stacks.iter().map(|s| s.back().unwrap()).collect()
     }
 
     type Output2 = String;
 
     /// Part 2 took 0.013784ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
-        let mut state = input.clone(); // get a copy of the state
-        for m in state.moves.clone() {
+        let state = input.clone(); // get a copy of the state
+        for m in &state.moves {
             state.move_crates_9001(m.amount, m.from, m.to);
         }
-        state.stacks.iter().map(|s| s.back().unwrap()).collect()
+        let stacks = state.stacks.borrow();
+        stacks.iter().map(|s| s.back().unwrap()).collect()
     }
 }
