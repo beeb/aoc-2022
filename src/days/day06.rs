18a473cc07d194ac14a6047d6ce241a18a674c2e
beeb <vbersier@gmail.com>
2022-12-06 06:57:51 +0100

    refactor(day6): cleanup

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -5,6 +5,15 @@ use crate::days::Day;
 
 pub struct Day06;
 
+fn find_unique_pattern(input: &[u8], len: usize) -> isize {
+    for (i, seq) in input.windows(len).enumerate() {
+        if seq.iter().all_unique() {
+            return (i + len) as isize;
+        }
+    }
+    -1 // sentinel value for not found
+}
+
 impl Day for Day06 {
     type Input = String;
 
@@ -12,25 +21,17 @@ impl Day for Day06 {
         Ok(("", String::from(input)))
     }
 
-    type Output1 = usize;
+    type Output1 = isize;
 
+    /// Part 1 took 0.0816ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        for (i, (a, b, c, d)) in input.chars().tuple_windows::<(_, _, _, _)>().enumerate() {
-            if [a, b, c, d].iter().all_unique() {
-                return i + 4;
-            }
-        }
-        0
+        find_unique_pattern(input.as_bytes(), 4)
     }
 
-    type Output2 = usize;
+    type Output2 = isize;
 
+    /// Part 2 took 0.3517ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
-        for (i, seq) in input.as_bytes().windows(14).enumerate() {
-            if seq.iter().all_unique() {
-                return i + 14;
-            }
-        }
-        0
+        find_unique_pattern(input.as_bytes(), 14)
     }
 }
