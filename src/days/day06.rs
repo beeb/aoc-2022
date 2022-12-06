64e2bc438ab271bcc9684053f4091404d75ce59d
beeb <vbersier@gmail.com>
2022-12-06 16:11:46 +0100

    feat(day6): added faster solution

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -5,10 +5,27 @@ use crate::days::Day;
 
 pub struct Day06;
 
+/// First easy and clean solution
+#[allow(unused)]
 fn pos_of_unique_pattern(input: &[u8], len: usize) -> usize {
     assert!(len > 0);
     for (i, seq) in input.windows(len).enumerate() {
         if seq.iter().all_unique() {
+            return i + len;
+        }
+    }
+    0 // sentinel value for not found (minimum is otherwise `len`)
+}
+
+/// Faster solution that doesn't rely on HashSet (which `all_unique` above uses internally)
+fn pos_of_unique_pattern2(input: &[u8], len: usize) -> usize {
+    assert!(len > 0);
+    for (i, seq) in input.windows(len).enumerate() {
+        if seq
+            .iter()
+            .enumerate()
+            .all(|(idx, c)| !seq[..idx].contains(c))
+        {
             return i + len;
         }
     }
@@ -24,15 +41,15 @@ impl Day for Day06 {
 
     type Output1 = usize;
 
-    /// Part 1 took 0.0816ms
+    /// Part 1 took 0.006ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        pos_of_unique_pattern(input.as_bytes(), 4)
+        pos_of_unique_pattern2(input.as_bytes(), 4)
     }
 
     type Output2 = usize;
 
-    /// Part 2 took 0.3517ms
+    /// Part 2 took 0.0231ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
-        pos_of_unique_pattern(input.as_bytes(), 14)
+        pos_of_unique_pattern2(input.as_bytes(), 14)
     }
 }
