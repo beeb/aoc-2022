06d35548a1165e4dec4042b1866d42dfb6821a0d
beeb <vbersier@gmail.com>
2022-12-06 07:02:20 +0100

    style(day6): rename fn

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -5,7 +5,7 @@ use crate::days::Day;
 
 pub struct Day06;
 
-fn find_unique_pattern(input: &[u8], len: usize) -> usize {
+fn indexof_unique_pattern(input: &[u8], len: usize) -> usize {
     assert!(len > 0);
     for (i, seq) in input.windows(len).enumerate() {
         if seq.iter().all_unique() {
@@ -26,13 +26,13 @@ impl Day for Day06 {
 
     /// Part 1 took 0.0816ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        find_unique_pattern(input.as_bytes(), 4)
+        indexof_unique_pattern(input.as_bytes(), 4)
     }
 
     type Output2 = usize;
 
     /// Part 2 took 0.3517ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
-        find_unique_pattern(input.as_bytes(), 14)
+        indexof_unique_pattern(input.as_bytes(), 14)
     }
 }
