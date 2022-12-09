3687c7cea8fd44342418f39d5c5c5e9f5e183777
beeb <vbersier@gmail.com>
2022-12-09 06:59:23 +0100

    feat(day9): parsing

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,20 +1,57 @@
-use nom::IResult;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{i64, line_ending},
+    combinator::map,
+    multi::separated_list0,
+    sequence::pair,
+    IResult,
+};
 
 use crate::days::Day;
 
 pub struct Day09;
 
-impl Day for Day09 {
-    type Input = String;
+#[derive(Debug)]
+pub enum Move {
+    Up(isize),
+    Right(isize),
+    Down(isize),
+    Left(isize),
+}
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+impl From<(&str, i64)> for Move {
+    fn from(p: (&str, i64)) -> Self {
+        match p.0 {
+            "U " => Self::Up(p.1 as isize),
+            "R " => Self::Right(p.1 as isize),
+            "D " => Self::Down(p.1 as isize),
+            "L " => Self::Left(p.1 as isize),
+            _ => unimplemented!("should not happen"),
+        }
+    }
+}
+
+impl Day for Day09 {
+    type Input = Vec<Move>;
+
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(
+            line_ending,
+            alt((
+                map(pair(tag("U "), i64), Move::from),
+                map(pair(tag("R "), i64), Move::from),
+                map(pair(tag("D "), i64), Move::from),
+                map(pair(tag("L "), i64), Move::from),
+            )),
+        )(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        println!("{input:?}");
+        0
     }
 
     type Output2 = usize;
