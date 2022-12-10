0fa538202ca7f14f48cd5e40f991eaba18e1c2bf
Valentin Bersier <vbersier@gmail.com>
2022-12-10 10:58:57 +0100

    refactor(day10)

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -38,7 +38,7 @@ impl Day for Day10 {
 
     /// Part 1 took 0.008851ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        let mut input = input.iter().rev().collect::<Vec<_>>(); // reverse so we can pop
+        let mut input = input.iter().rev().collect_vec(); // reverse so we can pop
         let mut x = 1; // the register
         let mut signal_sum = 0; // our output
         let mut add_val = 0; // temp var to hold the addx value over to the next cycle
@@ -75,7 +75,7 @@ impl Day for Day10 {
 
     /// Part 2 took 0.012493ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
-        let mut input = input.iter().rev().collect::<Vec<_>>(); // reverse so we can pop
+        let mut input = input.iter().rev().collect_vec(); // reverse so we can pop
         let mut crt: Vec<char> = vec!['.'; 40 * 6]; // 6 rows of 40 pixels
         let mut x = 1isize; // the register
         let mut add_val = 0; // temp var
