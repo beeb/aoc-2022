3ebeac9d66230e01d96029b784f1572377fac9c3
Valentin Bersier <vbersier@gmail.com>
2022-12-13 08:42:35 +0100

    docs: comments

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -12,6 +12,7 @@ use nom::{
 
 use crate::days::Day;
 
+/// Recursive enum for representing the packets
 #[derive(Debug, Clone)]
 pub enum PacketItem {
     Int(u8),
@@ -19,6 +20,7 @@ pub enum PacketItem {
 }
 
 impl PacketItem {
+    /// The dividiers are a list of list of a single int (2 or 6)
     pub fn is_divider(&self, val: u8) -> bool {
         match self {
             Self::Int(_) => false,
@@ -50,15 +52,15 @@ pub struct Packets {
 }
 
 impl Packets {
+    /// Return the two members as a vec (for later flattening)
     fn as_vec(&self) -> Vec<&PacketItem> {
         vec![&self.first, &self.second]
     }
 }
 
-fn parse_int(input: &str) -> IResult<&str, u8> {
-    u8(input)
-}
-
+/// Parse a list (square brackets on either side and a comma-separated list inside)
+///
+/// The items inside can themselves be either a list or an int (so a PacketItem enum)
 fn parse_list(input: &str) -> IResult<&str, Vec<PacketItem>> {
     preceded(
         char('['),
@@ -69,13 +71,14 @@ fn parse_list(input: &str) -> IResult<&str, Vec<PacketItem>> {
     )(input)
 }
 
+/// Parse an item, either a list or an int (recursive)
 fn parse_item(input: &str) -> IResult<&str, PacketItem> {
-    alt((
-        map(parse_int, PacketItem::Int),
-        map(parse_list, PacketItem::List),
-    ))(input)
+    alt((map(u8, PacketItem::Int), map(parse_list, PacketItem::List)))(input)
 }
 
+/// Check if two packets or packet items are in the right order
+///
+/// None represents equality. This is recursive also
 fn is_ordered(first: &PacketItem, second: &PacketItem) -> Option<bool> {
     match (first, second) {
         (PacketItem::Int(a), PacketItem::Int(b)) => {
@@ -123,11 +126,14 @@ impl Day for Day13 {
 
     type Output1 = usize;
 
+    /// Part 1 took 0.024534ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
+        // store the ordered status for each pair of packets
         let mut ordered: Vec<Option<bool>> = vec![None; input.len()];
         for (i, packets) in input.iter().enumerate() {
             ordered[i] = is_ordered(&packets.first, &packets.second);
         }
+        // get the positions where the ordering is `true` (+1) and sum them
         ordered
             .iter()
             .positions(|o| o.is_some() && o.unwrap())
@@ -137,19 +143,25 @@ impl Day for Day13 {
 
     type Output2 = usize;
 
+    /// Part 2 took 0.281495ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
+        // get a flat vec of all the packets
         let mut packets = input.iter().flat_map(|p| p.as_vec()).collect_vec();
+        // add the dividiers
         let div1 = PacketItem::List(vec![PacketItem::List(vec![PacketItem::Int(2)])]);
         let div2 = PacketItem::List(vec![PacketItem::List(vec![PacketItem::Int(6)])]);
         packets.push(&div1);
         packets.push(&div2);
+        // sort the list of packets with our function from part 1
         packets.sort_by(|a, b| match is_ordered(a, b) {
             Some(true) => Ordering::Less,
             Some(false) => Ordering::Greater,
             None => Ordering::Equal,
         });
+        // get the positions of each divider
         let first_div = packets.iter().position(|&p| p.is_divider(2)).unwrap();
         let second_div = packets.iter().position(|&p| p.is_divider(6)).unwrap();
+        // the result is the multiplication of both positions (+1)
         (first_div + 1) * (second_div + 1)
     }
 }
