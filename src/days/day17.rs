1536f13c9c5fb8a08d91d4dbcf04d72e20b79325
beeb <vbersier@gmail.com>
2022-12-17 13:37:29 +0100

    docs(day17): comments

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -17,7 +17,7 @@ pub enum Push {
 pub struct Piece {
     /// piece shape and lateral placement, starting from the bottom
     data: [u8; 4],
-    /// row containing the lowest part of the piece
+    /// row index of the lowest part of the piece
     z: usize,
 }
 
@@ -35,48 +35,57 @@ impl Piece {
         Self { data, z }
     }
 
-    /// the grid slice we consider is aligned with the piece data
+    /// Consider the part of the grid superposed with our piece
     fn move_left(&mut self, grid: &[u8]) -> &mut Self {
         let grid = &grid[self.z..self.z + 4];
+        // check that all rows of our piece would have no collisions if we move them
         let can_move = grid.iter().enumerate().all(|(i, row)| {
             let piece_row = self.data[i];
+            // try to move the piece row left, then BIT_AND with the grid, there should be no intersection = all zeroes
             (piece_row << 1 & row).count_ones() == 0
         });
         if !can_move {
             return self;
         }
+        // shift the piece left
         self.data.iter_mut().for_each(|r| {
             *r <<= 1;
         });
         self
     }
 
-    /// the grid slice we get is aligned with the piece data
+    /// Consider the part of the grid superposed with our piece
     fn move_right(&mut self, grid: &[u8]) -> &mut Self {
         let grid = &grid[self.z..self.z + 4];
+        // check that all rows of our piece would have no collisions if we move them
         let can_move = grid.iter().enumerate().all(|(i, row)| {
             let piece_row = self.data[i];
             if piece_row.trailing_ones() > 0 {
+                // we're already all the way to the right, we can't move right
                 return false;
             }
+            // try to move the piece row right, then BIT_AND with the grid, there should be no intersection = all zeroes
             (piece_row >> 1 & row).count_ones() == 0
         });
         if !can_move {
             return self;
         }
+        // shift the piece right
         self.data.iter_mut().for_each(|r| {
             *r >>= 1;
         });
         self
     }
 
-    /// the grid slice we consider comprises the one below the piece's z, and the bottom row of our piece
-    /// this should be enough to check for collisions, since pieces are increasing in section only on their lowest
+    /// The grid slice we consider comprises the one below the piece's `z`, and the bottom row of our piece.
+    /// This should be enough to check for collisions, since pieces are increasing in width at most on their lowest
     /// two rows
     fn can_move_down(&self, grid: &[u8]) -> bool {
         let grid = &grid[self.z - 1..=self.z];
         grid.iter().enumerate().all(|(i, row)| {
-            let piece_row = self.data[i]; // simulate if we moved down
+            // simulate if we moved down (i.e. piece row 0 (at index z) is in same position as grid row z-1)
+            let piece_row = self.data[i];
+            // check for collisions with the grid row
             (piece_row & row).count_ones() == 0
         })
     }
@@ -86,14 +95,17 @@ impl Piece {
         self
     }
 
+    /// The highest z position is the base position (`self.z`), plus the height of the piece, minus 1
     fn get_highest_z(&self) -> usize {
         let height = self
             .data
             .iter()
             .fold_while(0, |acc, row| {
                 if row.count_ones() > 0 {
+                    // as long as the part grid has `1` in the row, we keep counting the rows
                     Continue(acc + 1)
                 } else {
+                    // the row is all zeroes, we stop counting the rows
                     Done(acc)
                 }
             })
@@ -135,15 +147,22 @@ impl Day for Day17 {
 
     /// Part 1 took 0.175ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
+        // we make an infinite iterator for the air pushing actions
         let mut push = input.iter().cycle();
+        // we create a grid that should hold the full stack, drawing the wall on the left (1 means occupied)
+        let mut grid = [0b1000_0000_u8; 10_000];
         // at index 0 is the grid floor
-        let mut grid = [0b10000000u8; 10_000];
-        grid[0] = 0b11111111;
-        let mut highest_z = 0usize;
+        grid[0] = 0b1111_1111;
+        let mut highest_z = 0usize; // keep track of the stack height
+
+        // consider 2022 pieces
         for i in 0..2022 {
-            let kind = (i % 5) as u8;
-            let mut piece = Piece::new(kind, highest_z + 4);
+            let kind = (i % 5) as u8; // we cycle through the piece types
+            let mut piece = Piece::new(kind, highest_z + 4); // the piece is initially 4 above the top of the stack
+
+            // we loop until the part stops moving
             loop {
+                // apply the sideways push from the air
                 match push.next() {
                     Some(Push::Left) => {
                         piece.move_left(&grid);
@@ -155,13 +174,16 @@ impl Day for Day17 {
                         unreachable!("we cycle through the iterator indefinitely");
                     }
                 }
+                // part cannot move down, we move on to the next part
                 if !piece.can_move_down(&grid) {
                     break;
                 }
+                // part can move down, we proceed
                 piece.move_down();
             }
+            // update the highest position of the stack
             highest_z = piece.get_highest_z().max(highest_z);
-            // mutate grid
+            // mutate grid, adding our piece
             for (j, piece_row) in piece.data.into_iter().enumerate() {
                 grid[piece.z + j] |= piece_row;
             }
@@ -172,23 +194,30 @@ impl Day for Day17 {
     type Output2 = usize;
 
     fn part_2(input: &Self::Input) -> Self::Output2 {
+        // We want to be able to know where in the input sequence lies the current air push, hence enumerate()
+        // before the cycle() (so the index also cycles)
         let mut push = input.iter().enumerate().cycle();
-        let mut skipped = 0;
-        // at index 0 is the grid floor
+        let mut skipped = 0; // this variable will keep track of how much height we fast-forwarded
+
+        // Init our grid like last time, but a bit taller so we have time to notice a repeating pattern
         let mut grid = [0b10000000_u8; 100_000];
         grid[0] = 0b11111111;
         let mut highest_z = 0_usize;
-        // keep a cache of the stack height and piece index for a given combination of current piece type, air push
-        // index in the sequence, and state of the last few rows
+        // Keep a cache of the (stack height, piece index) for a given combination of:
+        //  - current piece type
+        //  - air push index in the input sequence
+        //  - and state of the last few top rows in the grid (64)
+        // If we get a cache hit, it means we are in a repeating pattern and we can extract the period
         let mut cache = HashMap::<Identifier, (usize, usize)>::new();
         let mut i = 0;
+        // We should iterate over 1e12 pieces
         while i < 1_000_000_000_000_usize {
             let kind = (i % 5) as u8;
             let mut piece = Piece::new(kind, highest_z + 4);
-            let mut last_push_idx;
+            let mut last_push_idx; // this will be used for the cache key
             loop {
-                let (push_idx, push_type) = push.next().unwrap();
-                last_push_idx = push_idx;
+                let (push_idx, push_type) = push.next().unwrap(); // get the next air push in the sequence (and its idx)
+                last_push_idx = push_idx; // save it in the parent scope
                 match push_type {
                     Push::Left => {
                         piece.move_left(&grid);
@@ -203,11 +232,16 @@ impl Day for Day17 {
                 piece.move_down();
             }
             highest_z = piece.get_highest_z().max(highest_z);
-            // mutate grid
             for (j, piece_row) in piece.data.into_iter().enumerate() {
                 grid[piece.z + j] |= piece_row;
             }
+
+            // Now we have finished our processing for this piece.
+            // Let's save the state in the cache (only when we have enough grid height to save, i.e. 64 rows).
+            // We only need to do this once, so when skipped > 0, we will not need to store cache anymore.
             if highest_z > 64 && skipped == 0 {
+                // The identifier is the state of the system, which will repeat itself eventually.
+                // We save the pattern of the to 64 rows in the grid
                 let identifier = Identifier {
                     piece_kind: kind,
                     push_idx: last_push_idx,
@@ -218,12 +252,16 @@ impl Day for Day17 {
                 if let Some((prev_height, prev_piece_idx)) =
                     cache.insert(identifier, (highest_z, i))
                 {
+                    // We got a cache hit! Let's compare the two states to know the period.
                     let height_diff = highest_z - prev_height;
                     let piece_diff = i - prev_piece_idx;
+                    // Now we know how many periods (cycles) we can skip without affecting the state/repetition
                     let skip_repeats = (1_000_000_000_000_usize - i) / piece_diff;
                     let skip_pieces = skip_repeats * piece_diff;
                     let skip_height = skip_repeats * height_diff;
+                    // We fast-forward our piece counter
                     i += skip_pieces;
+                    // We record how much height we skipped, which we will add to the final result to get the real value
                     skipped = skip_height;
                 }
             }
