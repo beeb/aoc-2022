3b3a16c7af8383a030ccceb6de2f8e823db1acbb
beeb <vbersier@gmail.com>
2022-12-18 19:49:53 +0100

    wip: day 18 using bfs for flood

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 use nom::{
     character::complete::{char, line_ending, u8},
     combinator::map,
@@ -13,6 +15,19 @@ pub struct Voxel {
     pub x: usize,
     pub y: usize,
     pub z: usize,
+}
+
+const DIRS: [(i8, i8, i8); 6] = [
+    (1, 0, 0),
+    (-1, 0, 0),
+    (0, 1, 0),
+    (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+];
+
+fn in_bounds(c: (i8, i8, i8)) -> bool {
+    c.0 >= 0 && c.0 < 20 && c.1 >= 0 && c.1 < 20 && c.2 >= 0 && c.2 < 20
 }
 
 pub struct Day18;
@@ -75,87 +90,41 @@ impl Day for Day18 {
     fn part_2(input: &Self::Input) -> Self::Output2 {
         // Volume of solidified lava
         let mut vol = [[[false; 20]; 20]; 20];
-        // We use a separate 3D array to store the outer volume, i.e. the flooded volume
         let mut flood = [[[false; 20]; 20]; 20];
-        flood[0][0][0] = true; // Seed the flooding volume in the corner, the water will expand from here
-
         // Populate the array from the input data
         for Voxel { x, y, z } in input.iter() {
             vol[*x][*y][*z] = true;
         }
-        let mut prev_flooded = 0;
-        let mut flooded;
-        // Run the flooding algorithm until the number of flooded cells doesn't change anymore
-        loop {
-            // Flood the full volume (we might miss some concavities on the first pass, even the second, etc.)
-            // so we run it on repeat until no more cells are flooded
-            for x in 0..20 {
-                for y in 0..20 {
-                    for z in 0..20 {
-                        if vol[x][y][z] || flood[x][y][z] {
-                            // if we have a solid here, it's obviously not part of the ouside.
-                            // if we already are flooded, we skip too.
-                            continue;
-                        }
-                        // Check if any neighbor was already "flooded" (and is not solid).
-                        // If any neighbor is flooded, so should "we" (at x, y, z).
-                        if x < 19 && flood[x + 1][y][z] && !vol[x + 1][y][z] {
-                            flood[x][y][z] = true;
-                            continue;
-                        }
-                        if x > 0 && flood[x - 1][y][z] && !vol[x - 1][y][z] {
-                            flood[x][y][z] = true;
-                            continue;
-                        }
-                        if y < 19 && flood[x][y + 1][z] && !vol[x][y + 1][z] {
-                            flood[x][y][z] = true;
-                            continue;
-                        }
-                        if y > 0 && flood[x][y - 1][z] && !vol[x][y - 1][z] {
-                            flood[x][y][z] = true;
-                            continue;
-                        }
-                        if z < 19 && flood[x][y][z + 1] && !vol[x][y][z + 1] {
-                            flood[x][y][z] = true;
-                            continue;
-                        }
-                        if z > 0 && flood[x][y][z - 1] && !vol[x][y][z - 1] {
-                            flood[x][y][z] = true;
-                            continue;
-                        }
-                    }
+        let mut visible = 0;
+        flood[0][0][0] = true;
+        let mut seen = HashSet::<(i8, i8, i8)>::new();
+        seen.insert((0, 0, 0));
+        let mut stack = VecDeque::<(i8, i8, i8)>::new();
+        stack.push_back((0, 0, 0));
+        while let Some(voxel) = stack.pop_front() {
+            for dir in DIRS {
+                let n = (voxel.0 + dir.0, voxel.1 + dir.1, voxel.2 + dir.2);
+                if !in_bounds(n) {
+                    continue;
+                }
+                if vol[n.0 as usize][n.1 as usize][n.2 as usize] {
+                    visible += 1;
+                } else if !seen.contains(&n) {
+                    flood[n.0 as usize][n.1 as usize][n.2 as usize] = true;
+                    stack.push_back(n);
+                    seen.insert(n);
                 }
             }
-            // total number of flooded cells
-            flooded = flood.iter().flatten().flatten().map(|&f| f as u8).sum();
-            if flooded > prev_flooded {
-                prev_flooded = flooded;
-            } else {
-                // we finished flooding, we can break out of the loop
-                break;
-            }
         }
-        // Same as part 1
-        let mut visible = 0;
-        for Voxel { x, y, z } in input.iter() {
-            if *x == 19 || flood[*x + 1][*y][*z] {
-                visible += 1;
+        for (y, sl) in flood[8].iter().enumerate() {
+            for &fl in sl {
+                print!("{}", fl as u8);
             }
-            if *x == 0 || flood[*x - 1][*y][*z] {
-                visible += 1;
+            print!(" ");
+            for v in vol[8][y] {
+                print!("{}", v as u8);
             }
-            if *y == 19 || flood[*x][*y + 1][*z] {
-                visible += 1;
-            }
-            if *y == 0 || flood[*x][*y - 1][*z] {
-                visible += 1;
-            }
-            if *z == 19 || flood[*x][*y][*z + 1] {
-                visible += 1;
-            }
-            if *z == 0 || flood[*x][*y][*z - 1] {
-                visible += 1;
-            }
+            println!();
         }
         visible
     }
