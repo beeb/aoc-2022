c5363947849e8da923064444760522f1bd5ea264
Valentin Bersier <vbersier@gmail.com>
2022-12-18 08:10:25 +0100

    feat(day18): part 1

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -10,9 +10,9 @@ use crate::days::Day;
 
 #[derive(Debug)]
 pub struct Voxel {
-    pub x: u8,
-    pub y: u8,
-    pub z: u8,
+    pub x: usize,
+    pub y: usize,
+    pub z: usize,
 }
 
 pub struct Day18;
@@ -25,7 +25,11 @@ impl Day for Day18 {
             line_ending,
             map(
                 tuple((u8, char(','), u8, char(','), u8)),
-                |(x, _, y, _, z)| Voxel { x, y, z },
+                |(x, _, y, _, z)| Voxel {
+                    x: x as usize,
+                    y: y as usize,
+                    z: z as usize,
+                },
             ),
         )(input)
     }
@@ -33,8 +37,32 @@ impl Day for Day18 {
     type Output1 = usize;
 
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        println!("{input:?}");
-        0
+        let mut vol = [[[false; 20]; 20]; 20];
+        let mut open_sides = 0;
+        for Voxel { x, y, z } in input.iter() {
+            vol[*x][*y][*z] = true;
+        }
+        for Voxel { x, y, z } in input.iter() {
+            if *x == 19 || !vol[*x + 1][*y][*z] {
+                open_sides += 1;
+            }
+            if *x == 0 || !vol[*x - 1][*y][*z] {
+                open_sides += 1;
+            }
+            if *y == 19 || !vol[*x][*y + 1][*z] {
+                open_sides += 1;
+            }
+            if *y == 0 || !vol[*x][*y - 1][*z] {
+                open_sides += 1;
+            }
+            if *z == 19 || !vol[*x][*y][*z + 1] {
+                open_sides += 1;
+            }
+            if *z == 0 || !vol[*x][*y][*z - 1] {
+                open_sides += 1;
+            }
+        }
+        open_sides
     }
 
     type Output2 = usize;
