059689d30323d1c73c141daa8d7bc9a47f447896
Valentin Bersier <vbersier@gmail.com>
2022-12-18 07:53:39 +0100

    feat(day18): parsing

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,20 +1,40 @@
-use nom::IResult;
+use nom::{
+    character::complete::{char, line_ending, u8},
+    combinator::map,
+    multi::separated_list0,
+    sequence::tuple,
+    IResult,
+};
 
 use crate::days::Day;
+
+#[derive(Debug)]
+pub struct Voxel {
+    pub x: u8,
+    pub y: u8,
+    pub z: u8,
+}
 
 pub struct Day18;
 
 impl Day for Day18 {
-    type Input = String;
+    type Input = Vec<Voxel>;
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(
+            line_ending,
+            map(
+                tuple((u8, char(','), u8, char(','), u8)),
+                |(x, _, y, _, z)| Voxel { x, y, z },
+            ),
+        )(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        println!("{input:?}");
+        0
     }
 
     type Output2 = usize;
