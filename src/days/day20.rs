0a9c18d6b15051db79c4d764fe603b77fe249b2b
beeb <vbersier@gmail.com>
2022-12-20 11:48:41 +0100

    feat(day20): part 2

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -10,6 +10,34 @@ const LENGTH: usize = 5000;
 
 pub struct Day20;
 
+fn mix(val: &mut [i64; LENGTH], idx: &mut [usize; LENGTH]) {
+    for i in 0..LENGTH {
+        let x = idx.iter().position(|&idx| idx == i).unwrap();
+        let shift = val[x] % (LENGTH as i64 - 1);
+        let dir = shift.signum();
+        let stop = (x as i64) + shift;
+        let mut j = x as i64;
+        while j != stop {
+            let j_wrap = j.rem_euclid(LENGTH as i64) as usize;
+            let j_next = (j + dir).rem_euclid(LENGTH as i64) as usize;
+            val.swap(j_wrap, j_next);
+            idx.swap(j_wrap, j_next);
+
+            j += dir;
+        }
+    }
+}
+
+fn calculate_output(val: &[i64; LENGTH]) -> i64 {
+    let zero_offset = val.iter().position(|&v| v == 0).unwrap();
+    (1..=3)
+        .map(|x| {
+            let idx = (zero_offset + 1000 * x) % LENGTH;
+            val[idx]
+        })
+        .sum()
+}
+
 impl Day for Day20 {
     type Input = Vec<i64>;
 
@@ -22,38 +50,21 @@ impl Day for Day20 {
     fn part_1(input: &Self::Input) -> Self::Output1 {
         let mut val: [i64; LENGTH] = input[..LENGTH].try_into().expect("wrong array length");
         let mut idx: [usize; LENGTH] = (0..LENGTH).collect::<Vec<_>>().try_into().unwrap();
-        for i in 0..LENGTH {
-            let x = idx.iter().position(|&idx| idx == i).unwrap();
-            let shift = val[x];
-            let dir = shift.signum();
-            let stop = (x as i64) + shift;
-            let mut j = x as i64;
-            while j != stop {
-                let j_wrap = j.rem_euclid(LENGTH as i64) as usize;
-                let j_next = (j + dir).rem_euclid(LENGTH as i64) as usize;
-                val.swap(j_wrap, j_next);
-                idx.swap(j_wrap, j_next);
-
-                j += dir;
-            }
-            let rot = stop.div_euclid(LENGTH as i64);
-            if rot < 0 {
-                val.rotate_left(rot.unsigned_abs() as usize);
-                idx.rotate_left(rot.unsigned_abs() as usize);
-            }
-        }
-        let zero_offset = val.iter().position(|&v| v == 0).unwrap();
-        (1..=3)
-            .map(|x| {
-                let idx = (zero_offset + 1000 * x) % LENGTH;
-                val[idx]
-            })
-            .sum()
+        mix(&mut val, &mut idx);
+        calculate_output(&val)
     }
 
-    type Output2 = usize;
+    type Output2 = i64;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let mut val: [i64; LENGTH] = input.iter().map(|v| v * 811589153).collect::<Vec<_>>()
+            [..LENGTH]
+            .try_into()
+            .expect("wrong array length");
+        let mut idx: [usize; LENGTH] = (0..LENGTH).collect::<Vec<_>>().try_into().unwrap();
+        for _ in 0..10 {
+            mix(&mut val, &mut idx);
+        }
+        calculate_output(&val)
     }
 }
