527b9c1d26ebf7dae9eed40b800a8fd2ae3a9a8a
beeb <vbersier@gmail.com>
2022-12-21 12:13:47 +0100

    perf(day21): add memoization

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -148,11 +148,19 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
 ///
 /// In part2, we want to return None in case we find the "humn" monkey
 /// If part2 is false, we will always return Some(value)
+///
+/// The cache is harmful to performance in part 1, so we only use it in part 2
 fn get_monkey_value(
+    cache: &mut HashMap<String, i64>,
     monkey: &Monkey,
     monkeys: &HashMap<String, Monkey>,
     part2: bool,
 ) -> Option<i64> {
+    if part2 {
+        if let Some(&val) = cache.get(&monkey.name) {
+            return Some(val);
+        }
+    }
     if part2 && monkey.name == "humn" {
         return None;
     }
@@ -161,19 +169,28 @@ fn get_monkey_value(
         MonkeyType::Operation(operation) => {
             let left_monkey = monkeys.get(&operation.left).unwrap();
             let right_monkey = monkeys.get(&operation.right).unwrap();
-            let left_value = get_monkey_value(left_monkey, monkeys, part2);
-            let right_value = get_monkey_value(right_monkey, monkeys, part2);
+            let left_value = get_monkey_value(cache, left_monkey, monkeys, part2);
+            let right_value = get_monkey_value(cache, right_monkey, monkeys, part2);
             if left_value.is_none() || right_value.is_none() {
                 // in part2, it's possible that one of those is None, in which case we propagate the None
                 return None;
             }
-            Some(operation.calc(left_value.unwrap(), right_value.unwrap()))
+            let res = operation.calc(left_value.unwrap(), right_value.unwrap());
+            if part2 {
+                cache.insert(monkey.name.clone(), res);
+            }
+            Some(res)
         }
     }
 }
 
 /// Recursively find the value for the undefined node "humn".
-fn find_humn_value(monkey: &Monkey, monkeys: &HashMap<String, Monkey>, value: i64) -> i64 {
+fn find_humn_value(
+    cache: &mut HashMap<String, i64>,
+    monkey: &Monkey,
+    monkeys: &HashMap<String, Monkey>,
+    value: i64,
+) -> i64 {
     // first, check which monkey type we have
     match &monkey.mtype {
         MonkeyType::Number(_) => value, // if we have a number monkey, it means we found "humn", we return the value
@@ -182,15 +199,15 @@ fn find_humn_value(monkey: &Monkey, monkeys: &HashMap<String, Monkey>, value: i6
             let left_monkey = monkeys.get(&operation.left).unwrap();
             let right_monkey = monkeys.get(&operation.right).unwrap();
             // one of the values should return "None" since it contains "humn" at some point
-            let left_val = get_monkey_value(left_monkey, monkeys, true);
-            let right_val = get_monkey_value(right_monkey, monkeys, true);
+            let left_val = get_monkey_value(cache, left_monkey, monkeys, true);
+            let right_val = get_monkey_value(cache, right_monkey, monkeys, true);
             // we check the type of operation, and solve the equation to know the value of the undefined branch
             let val = operation.solve(value, left_val, right_val);
             // now we know that the undefined branch (where its value is None) should have a value of "val"
             if left_val.is_none() {
-                find_humn_value(left_monkey, monkeys, val)
+                find_humn_value(cache, left_monkey, monkeys, val)
             } else {
-                find_humn_value(right_monkey, monkeys, val)
+                find_humn_value(cache, right_monkey, monkeys, val)
             }
         }
     }
@@ -214,14 +231,16 @@ impl Day for Day21 {
 
     /// Part 1 took 0.1176ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
+        let mut cache = HashMap::<String, i64>::new(); // not used in part 1
         let root = input.get("root").unwrap();
-        get_monkey_value(root, input, false).unwrap()
+        get_monkey_value(&mut cache, root, input, false).unwrap()
     }
 
     type Output2 = i64;
 
-    /// Part 2 took 1.7543ms
+    /// Part 2 took 0.4971ms
     fn part_2(input: &Self::Input) -> Self::Output2 {
+        let mut cache = HashMap::<String, i64>::new();
         // let's get root's two operands
         let root = input.get("root").unwrap();
         let MonkeyType::Operation(operation) = &root.mtype else {
@@ -230,15 +249,15 @@ impl Day for Day21 {
         let left_monkey = input.get(&operation.left).unwrap();
         let right_monkey = input.get(&operation.right).unwrap();
         // one of the branches should lead to the unknown "humn" value, in which case our function returns None
-        let left_val = get_monkey_value(left_monkey, input, true);
-        let right_val = get_monkey_value(right_monkey, input, true);
+        let left_val = get_monkey_value(&mut cache, left_monkey, input, true);
+        let right_val = get_monkey_value(&mut cache, right_monkey, input, true);
         // check if the left or right operand is none, and pass the other monkey to our recursive function
         if let Some(left_val) = left_val {
             // we know that the result of "right_monkey" should be "left_val" since they need to be equal
-            return find_humn_value(right_monkey, input, left_val);
+            return find_humn_value(&mut cache, right_monkey, input, left_val);
         } else if let Some(right_val) = right_val {
             // we know that the result of "left_monkey" should be "right_val" since they need to be equal
-            return find_humn_value(left_monkey, input, right_val);
+            return find_humn_value(&mut cache, left_monkey, input, right_val);
         }
         panic!("not found");
     }
